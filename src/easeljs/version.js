59c451def4263788f7748c7560c5994425ee16af
5kilian <5kilian@informatik.uni-hamburg.de>
2018-12-12 01:30:53 +0100

    Remove comments from src

--- src/easeljs/version.js
+++ src/easeljs/version.js
@@ -1,32 +1,15 @@
-/**
- * @module EaselJS
- */
 this.createjs = this.createjs || {};
 
-(function() {
-	"use strict";
+(function () {
+    'use strict';
 
-	/**
-	 * Static class holding library specific information such as the version and buildDate of
-	 * the library.
-	 * @class EaselJS
-	 **/
-	var s = createjs.EaselJS = createjs.EaselJS || {};
 
-	/**
-	 * The version string for this release.
-	 * @property version
-	 * @type String
-	 * @static
-	 **/
-	s.version = /*=version*/""; // injected by build process
+    var s = createjs.EaselJS = createjs.EaselJS || {};
 
-	/**
-	 * The build date for this release in UTC format.
-	 * @property buildDate
-	 * @type String
-	 * @static
-	 **/
-	s.buildDate = /*=date*/""; // injected by build process
+
+    s.version = '';
+
+
+    s.buildDate = '';
 
 })();
